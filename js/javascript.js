5482981a9231ba32e27c781a121c01fceb4bcb92
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-05 12:16:16 +0800

    upload to Github

---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    myURL = "http://usa10.fastcast4u.com:4800/currentsong?sid=1";
+    text = "" + myURL;
+    return text
+}
---
+++ listen.pls
@@ -0,0 +1,6 @@
+[playlist]
+NumberOfEntries=1
+File1=http://usa10.fastcast4u.com:4800/;
+Title1=MyKampus Radio
+Length1=-1
+Version=2
